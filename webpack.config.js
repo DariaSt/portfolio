824c0588bcfea9013f71b7af38b0c2d17ba041af
Daria <st.dashck@yandex.ru>
2019-10-08 17:20:56 +0300

    webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,11 @@ const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-
 
 module.exports = (env, argv) => {
+
   const isProductionBuild = argv.mode === "production";
-  const buildDir = "./docs";
+  const publicPath = './';
 
   const pcss = {
     test: /\.(p|post|)css$/,
@@ -33,6 +31,7 @@ module.exports = (env, argv) => {
     loader: "babel-loader",
     exclude: /node_modules/,
     options: {
+      presets: ['@babel/preset-env'],
       plugins: ["@babel/plugin-syntax-dynamic-import"]
     }
   };
@@ -85,16 +84,24 @@ module.exports = (env, argv) => {
     ]
   };
 
-  const configTemplate = {
+  const config = {
+    entry: {
+      main: "./src/main.js",
+      admin: "./src/admin/main.js"
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "[name].[hash].build.js",
+      publicPath: isProductionBuild ? publicPath : "",
+      chunkFilename: "[chunkhash].js"
+    },
     module: {
       rules: [pcss, vue, js, files, svg, pug]
     },
     resolve: {
       alias: {
         vue$: "vue/dist/vue.esm.js",
-        images: path.resolve(__dirname, "src/images"),
-        components: path.resolve(__dirname, "src/admin/components"),
-        "@": path.resolve(__dirname, "src/admin")
+        images: path.resolve(__dirname, "src/images")
       },
       extensions: ["*", ".js", ".vue", ".json"]
     },
@@ -107,7 +114,15 @@ module.exports = (env, argv) => {
       hints: false
     },
     plugins: [
-      new Dotenv(),
+      new HtmlWebpackPlugin({
+        template: "src/index.pug",
+        chunks: ["main"]
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/admin/index.pug",
+        filename: "admin/index.html",
+        chunks: ["admin"]
+      }),
       new SpriteLoaderPlugin({ plainSprite: true }),
       new VueLoaderPlugin()
     ],
@@ -115,8 +130,8 @@ module.exports = (env, argv) => {
   };
 
   if (isProductionBuild) {
-    configTemplate.devtool = "none";
-    configTemplate.plugins = (configTemplate.plugins || []).concat([
+    config.devtool = "none";
+    config.plugins = (config.plugins || []).concat([
       new webpack.DefinePlugin({
         "process.env": {
           NODE_ENV: '"production"'
@@ -128,9 +143,9 @@ module.exports = (env, argv) => {
       })
     ]);
 
-    configTemplate.optimization = {};
+    config.optimization = {};
 
-    configTemplate.optimization.minimizer = [
+    config.optimization.minimizer = [
       new TerserPlugin({
         cache: true,
         parallel: true,
@@ -140,44 +155,5 @@ module.exports = (env, argv) => {
     ];
   }
 
-  const mainConfig = {
-    ...configTemplate,
-    name: "main-config",
-    entry: {
-      main: ["@babel/polyfill", "./src/main.js"]
-    },
-    output: {
-      filename: "[name].build.js",
-      chunkFilename: "[chunkhash].js",
-      path: path.resolve(__dirname, `${buildDir}`)
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "src/index.pug"
-      }),
-      ...configTemplate.plugins
-    ]
-  };
-
-  const adminConfig = {
-    ...configTemplate,
-    name: "admin-config",
-    entry: {
-      admin: ["@babel/polyfill", "./src/admin/main.js"]
-    },
-    output: {
-      publicPath: isProductionBuild ? "" : "./admin/",
-      filename: "[name].build.js",
-      chunkFilename: "[chunkhash].js",
-      path: path.resolve(__dirname, `${buildDir}/admin`)
-    },
-    plugins: [
-      ...configTemplate.plugins,
-      new HtmlWebpackPlugin({
-        template: "src/admin/index.pug"
-      })
-    ]
-  };
-
-  return [mainConfig, adminConfig];
+  return config;
 };
